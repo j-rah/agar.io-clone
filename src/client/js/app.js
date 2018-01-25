3b4d8f0d55fab790203af3bf3cf71307e456fbc7
j-rah <net.null@hotmail.com>
2018-01-25 19:54:30 +1000

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -6,6 +6,8 @@ var global = require('./global');
 var playerNameInput = document.getElementById('playerNameInput');
 var socket;
 var reason;
+//myVars
+var house = document.getElementById('house');
 
 var debug = function(args) {
     if (console && console.log) {
@@ -319,9 +321,12 @@ function drawFood(food) {
     graph.strokeStyle = 'hsl(' + food.hue + ', 100%, 45%)';
     graph.fillStyle = 'hsl(' + food.hue + ', 100%, 50%)';
     graph.lineWidth = foodConfig.border;
-    drawCircle(food.x - player.x + global.screenWidth / 2,
+    /*drawCircle(food.x - player.x + global.screenWidth / 2,
                food.y - player.y + global.screenHeight / 2,
-               food.radius, global.foodSides);
+               food.radius, global.foodSides);*/
+    drawImage(house, food.x - player.x + global.screenWidth / 2,
+               food.y - player.y + global.screenHeight / 2,
+               food.radius, food.radius);
 }
 
 function drawVirus(virus) {
